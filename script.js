deca3fcde7642b14246df9738195a046f2afe227
Mykhailo <mishased12@gmail.com>
2024-02-24 10:55:59 +0100

    rename

---
+++ script.js
@@ -0,0 +1,8 @@
+if(alive){
+    console.log("Hello There, I am Misha, AKA MrSecret");
+    console.log("Currently writting code...");
+    coding_js();
+}
+else{
+    console.error("I'm busy with something else, 0_-");
+}
