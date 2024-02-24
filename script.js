0c2a3c8d02c57e21cdb36920dfb0542bac46c531
Mykhailo <mishased12@gmail.com>
2024-02-24 10:58:08 +0100

    smupdate

--- script.js
+++
@@ -1,8 +0,0 @@
-if(alive){
-    console.log("Hello There, I am Misha, AKA MrSecret");
-    console.log("Currently writting code...");
-    coding_js();
-}
-else{
-    console.error("I'm busy with something else, 0_-");
-}
